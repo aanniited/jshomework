f6b99c90cf2a482ca72cbd997bbf3f56ca213818
aanniited <153723475+aanniited@users.noreply.github.com>
2026-06-05 20:48:06 +0400

    Add files via upload
    8 პრაქტიკული დავალებები

--- main.js
+++ main.js
@@ -1,97 +1,52 @@
-function mySetTimeout(delay) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, delay);
-    });
+function expo(num, power, cb) {
+  if (power === 0) {
+    return cb(1);
+  }
+
+  return num * expo(num, power - 1, cb);
 }
 
+const result = expo(5, 3, (x) => x);
 
-function makeToys(delayTime) {
-    console.log(`სათამაშოების დამზადებას დასჭირდება ${delayTime / 1000} წამი)`);
-    
-    return mySetTimeout(delayTime).then(() => {
-        return new Promise((resolve, reject) => {
-            if (Math.random() > 0.1) {
-                resolve('Undefected');
-            } else {
-                reject('Defected');
-            }
-        });
+console.log(result); 
+
+// 2
+
+const postsContainer = document.getElementById("posts");
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((posts) => {
+    posts.forEach((post) => {
+      const postElement = document.createElement("div");
+
+      postElement.innerHTML = `
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+        <hr>
+      `;
+
+      postsContainer.appendChild(postElement);
     });
+  })
+  .catch((error) => {
+    console.log("Error:", error);
+  });
+
+//3
+async function deepCopyObject(obj) {
+  if (typeof obj !== "object" || obj === null) {
+    throw new Error("აქ უნდა იყოსობიექტი");
+  }
+
+  return structuredClone(obj);
 }
 
-
-function sellToys(status, delayTime) {
-   
-    return mySetTimeout(delayTime).then(() => {
-        return new Promise((resolve, reject) => {
-            if (status === 'Undefected') {
-                if (Math.random() > 0.7) {
-                    resolve('Toy has been sold');
-                } else {
-                    reject('Toy was unsuccessful');
-                }
-            } else {
-                reject('Cannot sell defected toy');
-            }
-        });
-    });
-}
+deepCopyObject({
+  name: "Ani",
+  age: 20,
+})
+  .then((copy) => console.log(copy))
+  .catch((error) => console.log(error.message));
 
 
-function deliverToys(saleStatus, delayTime) {
-    console.log(`მიწოდების პროცესი`);
-    
-    return mySetTimeout(delayTime).then(() => {
-        return new Promise((resolve, reject) => {
-            if (saleStatus === 'Toy has been sold') {
-                resolve('Toy has been successfully delivered to the customer');
-            } else {
-                reject('Delivery failed because the toy was not sold');
-            }
-        });
-    });
-}
-
-
-// then .catch
-makeToys(3000) 
-    .then((status) => {
-        console.log(`დამზადების სტატუსი: ${status}`);
-        return sellToys(status, 1000); 
-    })
-    .then((saleStatus) => {
-        console.log(`გაყიდვის სტატუსი: ${saleStatus}`);
-        return deliverToys(saleStatus, 2000); 
-    })
-    .then((deliveryResult) => {
-        console.log(deliveryResult); 
-    })
-    .catch((err) => {
-        console.log(`${err}`);
-    });
-
-
-    // async await
-async function startToyBusiness() {
-    try {
-        
-        const status = await makeToys(3000);
-        console.log(`დამზადების სტატუსი: ${status}`);
-        
-        
-        const saleStatus = await sellToys(status, 1000);
-        console.log(`გაყიდვის სტატუსი: ${saleStatus}`);
-        
-       
-        const deliveryResult = await deliverToys(saleStatus, 2000);
-        console.log(deliveryResult);
-        
-    } catch (err) {
-        
-        console.log(`${err}`);
-    }
-}
-
-startToyBusiness();
