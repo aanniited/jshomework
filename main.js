c6f4602bd2262a3b571c465e025eb761eabd06b5
aanniited <153723475+aanniited@users.noreply.github.com>
2026-06-06 23:44:05 +0400

    Add files via upload
    9. დავალება

--- main.js
+++ main.js
@@ -1,52 +1,9 @@
-function expo(num, power, cb) {
-  if (power === 0) {
-    return cb(1);
-  }
+const isNumber= require("is-number");
 
-  return num * expo(num, power - 1, cb);
-}
-
-const result = expo(5, 3, (x) => x);
-
-console.log(result); 
-
-// 2
-
-const postsContainer = document.getElementById("posts");
-
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((response) => response.json())
-  .then((posts) => {
-    posts.forEach((post) => {
-      const postElement = document.createElement("div");
-
-      postElement.innerHTML = `
-        <h2>${post.title}</h2>
-        <p>${post.body}</p>
-        <hr>
-      `;
-
-      postsContainer.appendChild(postElement);
-    });
-  })
-  .catch((error) => {
-    console.log("Error:", error);
-  });
-
-//3
-async function deepCopyObject(obj) {
-  if (typeof obj !== "object" || obj === null) {
-    throw new Error("აქ უნდა იყოსობიექტი");
-  }
-
-  return structuredClone(obj);
-}
-
-deepCopyObject({
-  name: "Ani",
-  age: 20,
-})
-  .then((copy) => console.log(copy))
-  .catch((error) => console.log(error.message));
+console.log(isNumber(125))
 
 
+const validator = require("validator");
+
+console.log(validator.isEmail("test@test.com"));
+console.log(validator.isEmail("abcDE123"));
